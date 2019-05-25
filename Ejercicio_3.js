20d9da0316f1c3b6fb073cfbb5707894213aa722
Sebastian Subizar <mahoushonen@99.200-57-1.bestelclientes.com.mx>
2019-05-24 21:46:46 -0500

    clase 5 ejercicios

---
+++ Ejercicio_3.js
@@ -0,0 +1,7 @@
+        const nombre = console.log("nombre")
+        const password = console.log("password")
+        if (password === "sebastian" && nombre === "sebastian") {
+            "Erest tu!"
+        } else {
+            "Incorrecto"
+        }
